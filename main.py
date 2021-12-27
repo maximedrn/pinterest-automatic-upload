2a4d05e02c225c6d372bb27ce1ae244b12a12152
Maxime Dréan <91475935+maximedrn@users.noreply.github.com>
2021-12-27 18:15:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait as WDW
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 # Python default import.
